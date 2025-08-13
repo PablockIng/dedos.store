a2ddab88958b0c78fc6ea8741b3d6f7e6c1e86ab
Pablo <ininde.bogota@gmail.com>
2025-08-12 23:56:03 -0500

    segundo commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Página Home</h1>
-      <p>.Bienvenido a la página principal.</p>
+      <p>...Bienvenido a la página principal...</p>
     </div>
   );
 }
